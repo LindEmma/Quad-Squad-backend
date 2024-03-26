85d95464b0acd742103ad5e1985735e28eff7e80
Emma <elune.lind@gmail.com>
2024-03-26 22:23:53 +0100

    added code to  server2 so that it collects timereports as well

--- Server2.js
+++ Server2.js
@@ -2,40 +2,69 @@ const express = require("express");
 const { Client } = require("@notionhq/client");
 const cors = require("cors");
 const port = 8000;
+require("dotenv").config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 const authToken = "secret_wE9pW5CmFqoJCsy3kyZTdqyVj7MGfDuKHu5ndKHIfwP";
-const notionDbID = "a00eba2577284abf93bd7f0e5dbc3340";
+const projectsID = process.env.NOTION_PROJECT_DATABASE_ID;
+const timereportsID = process.env.NOTION_TIMEREPORTS_DATABASE_ID;
 
 app.post("/ActiveProjects", async (req, res) => {
   const { storedUserID } = req.body;
-  console.log('User ID sent from front-end:', storedUserID);
-  
+  console.log("User ID sent from front-end:", storedUserID);
+
   const notion = new Client({ auth: authToken });
   try {
-    // Hämta alla projekt från Notion-databasen och filtrera baserat på användar-ID
-    const response = await notion.databases.query({
-      database_id: notionDbID,
+    // Collects all projects connected to logged in users id
+    const projectResponse = await notion.databases.query({
+      database_id: projectsID,
       filter: {
         property: "👤People",
         relation: {
-          contains: storedUserID
-        }
-      }
+          contains: storedUserID,
+        },
+      },
     });
 
-    res.json(response);
-    console.log(response);
+    console.log("Response from Notion API:", projectResponse);
+    const projectData = projectResponse.results;
+
+    for (const project of projectData) {
+      //collects all timereports connected to user id and correct project id
+      const timereportResponse = await notion.databases.query({
+        database_id: timereportsID,
+        filter: {
+          and: [
+            {
+              property: "Projects",
+              relation: {
+                contains: project.id,
+              },
+            },
+            {
+              property: "Person",
+              relation: {
+                contains: storedUserID,
+              },
+            },
+          ],
+        },
+      });
+
+      console.log("tidsrapport:", timereportResponse);
+      project.timereports = timereportResponse.results;
+    }
+
+    res.json({ results: projectData });
     console.log("Success");
   } catch (error) {
     console.error("Error:", error);
     res.status(500).send("Internal Server Error");
   }
 });
-
 app.listen(port, () => {
   console.log(`Server listening on port ${port}!`);
 });
